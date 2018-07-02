2315190a73f9804b88293cdb0396035254342c11
Thura Soe <trickleith@icloud.com>
2018-07-02 21:57:27 +0630

    test

--- app/src/main/java/com/trickleith/groupsketch/Intro.java
+++ app/src/main/java/com/trickleith/groupsketch/Intro.java
@@ -9,5 +9,7 @@ public class Intro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        //test commit
     }
 }
